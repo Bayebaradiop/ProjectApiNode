c0c9e1e3440da1e929948e6a6c80e70d0b6dec9a
bara <bayebara2000@gmail..com>
2025-09-01 00:54:11 +0000

    reglage erreur

--- src/routes/tag.routes.ts
+++ src/routes/tag.routes.ts
@@ -1,16 +1,14 @@
 
 import { Router } from 'express';
 import { createTag, deleteTag, getAllTags, getTagById, updateTag } from '../controllers/tag.controller';
-// import { validate } from '../validators/validate.middleware';
-// import { createTagSchema, updateTagSchema, tagIdSchema } from '../validators/tag.validator';
 
 const router = Router();
 
 // Routes
 router.get('/', getAllTags);
 router.get('/:id', getTagById);
-router.post('/', createTag); // add validate(createTagSchema) if middleware available
-router.put('/:id', updateTag); // add validate(updateTagSchema) if middleware available
+router.post('/', createTag); 
+router.put('/:id', updateTag); 
 router.delete('/:id', deleteTag);
 
 export default router;
